82406ae0eb0938fc3876578cfd2f3a5ac08f9c60
Vlary <manoelfranciscose@gmail.com>
2021-01-13 21:41:35 -0300

    first commit

---
+++ src/components/Question.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="question">
+    <span>{{ question.text }}</span>
+    <ul class="answers">
+      <li
+        v-for="(answer, i) in question.answers"
+        :key="answer.text"
+        class="fade-out-right"
+        @click="$emit('answered', answer.correct)"
+      >
+        <span class="number">{{ i + 1 }}</span>
+        <span class="text">{{ answer.text }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    question: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.question {
+  height: 400px;
+  color: #000;
+  background-color: #fff;
+  width: 100%;
+  border-radius: 20px;
+  font-size: 2.5rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  border-left: rgb(163, 155, 155) 1px solid;
+}
+
+ul.answers {
+  font-size: 2rem;
+  padding: 0;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+
+.answers li {
+  margin: 20px;
+  background: #4dfd07;
+  background: -webkit-linear-gradient(to right, #ffe000, #799f0c, #4dfd07);
+  background: linear-gradient(to right, #ffe000, #799f0c, #4dfd07);
+
+  border-radius: 8px;
+  width: 40%;
+
+  display: flex;
+}
+
+.answers .number {
+  padding: 10px;
+  background-color: #1e9c31;
+  color: #fff;
+  flex-basis: 30px;
+  border-top-left-radius: 8px;
+  border-bottom-left-radius: 8px;
+}
+
+.answers .text {
+  flex: 1;
+  align-self: center;
+}
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="result" :class="{ wrong: !result }">
+    <span v-if="result" 
+    BGSOUND SRC="./palmas.mp3" loop="-1">Resposta Certa! :)</span>
+    <span v-else>Resposta Errada! :(</span>
+    <hr />
+    <button @click="$emit('confirmed')">Próxima Pergunta</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    result: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.result {
+  height: 400px;
+  color: #000;
+  background-color: #fff;
+  width: 120%;
+  border-radius: 20px;
+  font-size: 5.5rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #89c454;
+  color: #30863d;
+}
+
+.result hr {
+  color: #fff;
+}
+
+.result.wrong {
+  background-color: #ff0400;
+  color: #ffffff;
+}
+
+.result button {
+  outline: none;
+  align-self: center;
+  padding: 10px 20px;
+  font-size: 1.7rem;
+  border-radius: 8px;
+  background-color: #fff;
+  color: #30863d;
+  font-weight: 600;
+}
+
+.result.wrong button {
+  color: #bb2814;
+}
+</style>
